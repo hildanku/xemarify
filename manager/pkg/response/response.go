4ae4da6d034bde653032f3d3edd5b0652f0bf3d3
hildanku <hildankutomo@gmail.com>
2026-03-07 02:29:10 +0700

    chore(response): remove response body `code` 'cause redundant

--- manager/pkg/response/response.go
+++ manager/pkg/response/response.go
@@ -5,7 +5,6 @@ import (
 )
 
 type JSONResponse struct {
-	Code    int    `json:"code"`
 	Message string `json:"message"`
 	Data    any    `json:"data,omitempty"`
 }
@@ -13,7 +12,6 @@ type JSONResponse struct {
 // Write is a helper that writes a JSON response with a consistent structure.
 func Write(c *gin.Context, code int, message string, data any) {
 	c.JSON(code, JSONResponse{
-		Code:    code,
 		Message: message,
 		Data:    data,
 	})
